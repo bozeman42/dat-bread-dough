17d2046dcb14c5ffd54bd8b85d783a72261003bd
Aaron Kvarnlov-Leverty <bozeman42@gmail.com>
2019-03-13 12:09:04 -0500

    updated title, favicon, and added a description

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ class App extends Component {
         <header className='dbd-header'>
           <h1>Dat Bread, Dough!</h1>
         </header>
+        <div className='Description'>
+        <p>
+          This calculator is used to find the appropriate water temperature for
+          your dough based upon the calculation provided in
+          Bonnie Ohara's <em>Bread Baking for Beginners</em>.
+        </p>
+        </div>
         <div>
           <Calculator />
         </div>
